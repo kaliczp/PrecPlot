89b322c26dcdb21feefd82458dcf1484467099f7
Péter Kalicz <kaliczp@gmail.com>
2019-03-30 08:20:04 +0100

    Determine the highest value to plot
    Now during the read ts.end.values collect the endpoint every cumulative
    hyetograph. After the pre-processing the maxima is selected and applied
    during the plot.

--- prec.cumulative.R
+++ prec.cumulative.R
@@ -1,18 +1,28 @@
 prec.cumulative <- function(time.series=c("Boreas.xts", "c1.xts", "hhm.xts"), time.lim = tttime) {
     require(xts)
     ts.num <- length(time.series)
+    ts.end.values <- numeric()
     ## Read time-series and pre-process
     for(act.ts.num in 1:ts.num) {
         ## Read in and cut
         ts.readed.window <- get(time.series[act.ts.num])[time.lim]
         ## Cumulate coredata
         ts.cumulated.coredata <- cumsum(coredata(ts.readed.window))
+        ## Collect end value
+        ts.length <- length(ts.cumulated.coredata)
+        ts.end.values <- c(ts.end.values, ts.cumulated.coredata[ts.length])
         ## Generate new zoo series
         ts.cumulated <- zoo(ts.cumulated.coredata, time(ts.readed.window))
         assign(paste("ts", act.ts.num, sep="."), ts.cumulated)
     }
+
+    ## Select highest value for y-axis limit
+    maximal.prec <- max(ts.end.values)
+
     ## Plot time-series
-    plot.zoo(zoo(cumsum(coredata(Boreas.xts[time.lim])),time(Boreas.xts[time.lim])), xlab="",ylab="Precipitation [mm]", type = "n")
+    plot(ts.1, xlab="",ylab="Precipitation [mm]", type = "n",
+             ylim = c(0, maximal.prec)
+             )
     grid(nx = NA, ny = NULL)
     for(act.ts.num in 1:ts.num) {
         ts.to.plot <- get(paste("ts", act.ts.num, sep="."))
