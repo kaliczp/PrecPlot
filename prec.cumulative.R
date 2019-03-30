1ec484699da06222c960eb2e7408600262be286b
Péter Kalicz <kaliczp@gmail.com>
2019-03-30 07:39:25 +0100

    Works now with arbitrary number of time-series
    Now the list in the arguments can be changed freely. The plotted
    lines is determined based on the time-sereies list.

--- prec.cumulative.R
+++ prec.cumulative.R
@@ -1,14 +1,16 @@
 prec.cumulative <- function(time.series=c("Boreas.xts", "c1.xts", "hhm.xts"), time.lim = tttime) {
     require(xts)
+    ts.num <- length(time.series)
     ## Read time-series
-    ts.1 <- get(time.series[1])
-    ts.2 <- get(time.series[2])
-    ts.3 <- get(time.series[3])
+    for(act.ts.num in 1:ts.num) {
+        assign(paste("ts", act.ts.num, sep="."), get(time.series[act.ts.num]))
+    }
     ## Plot time-series
     plot.zoo(zoo(cumsum(coredata(Boreas.xts[time.lim])),time(Boreas.xts[time.lim])), xlab="",ylab="Precipitation [mm]", type = "n")
     grid(nx = NA, ny = NULL)
-    lines(zoo(cumsum(coredata(ts.1[time.lim])),time(ts.1[time.lim])),lwd=2, col=1)
-    lines(zoo(cumsum(coredata(ts.2[time.lim])),time(ts.2[time.lim])), lwd=2, col=2)
-    lines(zoo(cumsum(coredata(ts.3[time.lim])),time(ts.3[time.lim])), lwd=2, col=4)
+    for(act.ts.num in 1:ts.num) {
+        ts.to.plot <- get(paste("ts", act.ts.num, sep="."))[time.lim]
+        lines(zoo(cumsum(coredata(ts.to.plot)),time(ts.to.plot)),lwd=2, col=act.ts.num)
+    }
     legend("bottomright", legend=c("Samp.: 10 min, Res.: 0.1 mm", "Samp.: 1 min, Res.: 0.1 mm", "Samp.: 1 sec, Res.: 0.5 mm"), lwd = 2, col=c(1,2,4))
 }
