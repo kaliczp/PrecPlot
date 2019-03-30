c5eb5431b9db84e1b888818f97b772442116e5c8
Péter Kalicz <kaliczp@gmail.com>
2019-03-30 09:18:14 +0100

    Legend generated based on time.series names
    Earlier fixed legend is changed to generated. Legend is generated
    based on the time.series vector, which is an arugment.

--- prec.cumulative.R
+++ prec.cumulative.R
@@ -29,5 +29,5 @@ prec.cumulative <- function(time.series=c("Boreas.xts", "c1.xts", "hhm.xts"), ti
         ts.to.plot <- get(paste("ts", act.ts.num, sep="."))
         lines(ts.to.plot, lwd=2, col=act.ts.num)
     }
-    legend("bottomright", legend=c("Samp.: 10 min, Res.: 0.1 mm", "Samp.: 1 min, Res.: 0.1 mm", "Samp.: 1 sec, Res.: 0.5 mm"), lwd = 2, col=c(1,2,4))
+    legend("bottomright", legend=time.series, lwd = 2, col=1:length(time.series))
 }
